c41dd8d8e496545be1f42000a18f917f6fb964b4
Sai Tejaswini <S533726@nwmissouri.edu>
2019-04-08 15:25:51 -0500

    commit

---
+++ app/app/Models/DeveloperModel.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Developer.Models
+
+{
+    public class Developer
+{
+    public int Id { get; set; }
+
+    public string Given { get; set; }
+
+    public string Family { get; set; }
+
+    public string GraduationTerm { get; set; }
+
+    public string DesiredPosition { get; set; }
+
+    public string Skills { get; set; }
+
+    public int Rating { get; set; }
+
+    [Display(Name = "Created Date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime CreatedDate { get; set; }
+
+}
+}
